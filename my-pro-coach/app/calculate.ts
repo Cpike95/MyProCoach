d9a0f6abc430abacc6b7a2c123442433cf401cb3
Curtis Pike <58271540+Cpike95@users.noreply.github.com>
2025-03-18 20:17:13 -0500

    Fix calculate call with main page

--- my-pro-coach/app/calculate.ts
+++ my-pro-coach/app/calculate.ts
@@ -4,14 +4,11 @@ import { calculateMacros } from "@/lib/macros";
 import { generateMealPlan } from "@/lib/meals";
 import {  generateWorkoutPlan } from "@/lib/workouts";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("hi")
-  if (req.method !== "POST") return res.status(405).end();
-  const data: UserData = req.body;
+export default function handler(data: UserData) {
 
   const macros = calculateMacros(data);
   const mealPlan = generateMealPlan(data, macros);
   const workoutPlan = generateWorkoutPlan(data);
 
-  res.status(200).json({ macros, mealPlan, workoutPlan });
+  return {macros, mealPlan, workoutPlan}
 }
